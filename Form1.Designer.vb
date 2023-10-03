7c7f79bf718f479942274fbde22f8dce8746a6db
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-03 10:49:27 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnJackyZungu = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(357, 271)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(100, 54)
+        Me.btnClose.TabIndex = 0
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnJackyZungu
+        '
+        Me.btnJackyZungu.Location = New System.Drawing.Point(318, 145)
+        Me.btnJackyZungu.Name = "btnJackyZungu"
+        Me.btnJackyZungu.Size = New System.Drawing.Size(159, 60)
+        Me.btnJackyZungu.TabIndex = 1
+        Me.btnJackyZungu.Text = "Jacky Zungu"
+        Me.btnJackyZungu.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnJackyZungu)
+        Me.Controls.Add(Me.btnClose)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnJackyZungu As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "My first App in 2023"
+        Me.BackColor = Color.BlueViolet
+        Me.MaximizeBox = True
+        Me.MinimizeBox = False
+        Me.Size = New Size(700, 500)
+        Me.Opacity = 0.85  'makes it bit transparent
+        Me.CenterToParent() 'centers the execution box
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+        Me.BackColor = Color.Beige
+    End Sub
+
+    Private Sub btnJackyZungu_Click(sender As Object, e As EventArgs) Handles btnJackyZungu.Click
+        MsgBox("this is jacky",, "Hi") 'this customise the box to display a message after click
+    End Sub
+End Class
